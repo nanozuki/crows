0b27a7c0f0d9b886baca48fbff290c40e9d9f075
Nanozuki Crows <nanozuki.crows@gmail.com>
2019-05-05 13:35:55 +0800

    [fish] add env define

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,14 @@
 set -x GOPATH $HOME/.go
 set -x PATH /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin $GOPATH/bin
+set -x XDG_CONFIG_HOME "$HOME/.config/"
+
+set -x XDG_CACHE_HOME "$HOME/.cache/"
+set -x XDG_DATA_HOME "$HOME/.data/"
+
+set -x HOMEBREW_GITHUB_API_TOKEN 5e3a008b84a0e66ecd4110d9a59753129f5a0bc4
+
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK /Users/crows/.gnupg/S.gpg-agent.ssh
 
 alias tree="tree -L 2"
 alias psg="ps aux | grep"
