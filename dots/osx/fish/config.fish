c1a6ea721891f5f4c129043418cc24619fe5013e
Nanozuki Crows <nanozuki.crows@gmail.com>
2020-01-06 09:05:11 +0800

    [bootstrap] add bootstrap scripts

--- dots/osx/fish/config.fish
+++ dots/osx/fish/config.fish
@@ -5,7 +5,7 @@ set -x PATH /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin $GOPATH
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent.ssh
 
-alias tree="tree -L 2"
+alias tree="tree -L 3"
 alias psg="ps aux | grep"
 
 if type -q nvim
@@ -16,7 +16,6 @@ if type -q direnv
     direnv hook fish | source
 end
 
-#mac
 alias appadd="brew install"
 alias appup="brew update && brew upgrade && brew cleanup"
 alias appsch="brew search"
