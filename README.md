f855c304af0e0f5ad076365bfba6d6a8ab42a92d
Nanozuki Crows <nanozuki.crows@gmail.com>
2020-01-06 09:05:11 +0800

    [bootstrap] add bootstrap scripts

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # CrowsEnv
 
 Develop Enviroment of Nanozuki Crows
+
+## bootstrap
+
+put configure files to `$HOME/.config/CrowsEnv`, the structure of config like this:
+
+```bash
+├── git
+│   ├── config_local
+│   └── config_work
+├── gpgkeys
+│   ├── crows_pub.gpg
+│   ├── crows_sec.gpg
+│   ├── work_pub.gpg
+│   └── work_sec.gpg
+└── sshkeys
+    ├── config
+    ├── id_ed25519
+    ├── id_ed25519.pub
+    ├── id_work
+    └── id_work.pub
+```
+
+and then:
+
+```bash
+curl -L https://raw.githubusercontent.com/nanozuki/CrowsEnv/master/crows-env.sh | bash -s bootstrap
+```
+
+## update
+
+modify files or do git operator in $HOME/.data/CrowsEnv, and: 
+
+```bash
+crows-env.sh update
+```
---
+++ dots/arch/fish/config.fish
@@ -0,0 +1,29 @@
+set -x GOPATH $HOME/.go
+set -x CARGO_HOME $HOME/.cargo
+set -x PATH /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin $GOPATH/bin $CARGO_HOME/bin
+
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+
+alias tree="tree -L 3"
+alias psg="ps aux | grep"
+
+if type -q nvim
+    alias vim="nvim"
+    alias vimdiff="nvim -d"
+end
+if type -q direnv
+    direnv hook fish | source
+end
+
+alias appadd="sudo pacman -S"
+alias appup="sudo pacman -Syu"
+alias appsch="pacman -Ss"
+alias appdel="sudo pacman -Rs"
+alias appclean="sudo pacman -Scc && pacman -Qdtq | pacman -Rs -"
+alias appinfo="pacman -Si"
+
+alias appextadd="yay -S"
+alias appextup="yay -Syu"
+alias appextsch="yay -Ss"
+alias appextdel="yay -Rs"
