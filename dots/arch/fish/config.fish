57cdc7b30becc5ab250d1e4dff26afc772005bc4
Nanozuki Crows <nanozuki.crows@gmail.com>
2020-01-08 19:15:37 +0800

    add bootstrap scripts

--- dots/arch/fish/config.fish
+++ dots/arch/fish/config.fish
@@ -3,9 +3,9 @@ set -x CARGO_HOME $HOME/.cargo
 set -x PATH /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin $GOPATH/bin $CARGO_HOME/bin
 
 set -x GPG_TTY (tty)
-#set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent.ssh
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
 
-alias tree="tree -L 2"
+alias tree="tree -L 3"
 alias psg="ps aux | grep"
 
 if type -q nvim
@@ -16,20 +16,6 @@ if type -q direnv
     direnv hook fish | source
 end
 
-# mac
-#alias appadd="brew install"
-#alias appup="brew update && brew upgrade && brew cleanup"
-#alias appsch="brew search"
-#alias appdel="brew uninstall"
-#alias appclean="brew cleanup"
-#alias appinfo="brew info"
-#
-#alias appextadd="brew cask install"
-#alias appextsch="brew cask search"
-#alias appextdel="brew cask uninstall"
-#alias appextup="brew cask upgrade"
-
-# arch
 alias appadd="sudo pacman -S"
 alias appup="sudo pacman -Syu"
 alias appsch="pacman -Ss"
---
+++ dots/osx/nvim/init.vim
@@ -0,0 +1,7 @@
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+let g:python3_host_prog = '/usr/local/bin/python3'
+let g:python_host_prog  = '/usr/local/bin/python2'
+set termguicolors
+"set shell=/bin/sh
+source ~/.vimrc
