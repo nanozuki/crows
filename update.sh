1235d3f016a09087e8f602425f6b6b833ef3bfda
Nanozuki Crows <nanozuki.crows@gmail.com>
2020-02-18 18:52:48 +0800

    [scripts] improve update and upgrade script

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 if [[ "$platform" == "osx" ]]; then
     brew update && brew upgrade && brew cleanup
     brew cask upgrade
-    pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+    pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
 elif [[ "$platform" == "arch" ]]; then
     sudo pacman -Syu
     yay -Syu
@@ -11,4 +11,4 @@ fi
 
 rustup update
 yarn global upgrade
-vim temp.go +PlugUpgrade +PlugUpdate +GoUpdateBinaries +qall
+nvim temp.go +PlugUpgrade +PlugUpdate +GoUpdateBinaries +qall
