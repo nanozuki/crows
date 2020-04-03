2179921bbf71032333af5cd861e1f90635500560
Nanozuki Crows <nanozuki.crows@gmail.com>
2020-04-03 21:00:47 +0800

    [scripts] update scripts

--- update.sh
+++ update.sh
@@ -3,12 +3,13 @@ if [[ "$platform" == "osx" ]]; then
     brew update && brew upgrade && brew cleanup
     brew cask upgrade
     pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
+    yarn global upgrade
 elif [[ "$platform" == "arch" ]]; then
     sudo pacman -Syu
     yay -Syu
     pip list --user --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install --user -U
+    sudo yarn global upgrade
 fi
 
 rustup update
-yarn global upgrade
 nvim temp.go +PlugUpgrade +PlugUpdate +GoUpdateBinaries +qall
