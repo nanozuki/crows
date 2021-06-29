c6e81b0f21c54906f174bdf4e2348fd53d0e77be
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-06-30 00:47:07 +0800

    [tmux] use vim plug to generate theme

--- scripts/tmux.fish
+++ scripts/tmux.fish
@@ -12,19 +12,4 @@ function sync_tmux
     for file in (ls $dots/tmux/)
         ln -sf $dots/tmux/$file $config/tmux/$file
     end
-
-    install_tpm
-    update_tpm
-end
-
-function install_tpm
-    if not test -d $config/tmux/plugins/tpm
-        git clone https://github.com/tmux-plugins/tpm $config/tmux/plugins/tpm
-    end
-    $config/tmux/plugins/tpm/bin/install_plugins
-    $config/tmux/plugins/tpm/bin/clean_plugins
-end
-
-function update_tpm
-    $config/tmux/plugins/tpm/bin/update_plugins all
 end
