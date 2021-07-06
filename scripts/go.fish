87d57a83d97e5a70e21e047fee63ccaf5bf6e915
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-07-06 18:47:29 +0800

    [go] improve nvim settings and scripts for go

--- scripts/go.fish
+++ scripts/go.fish
@@ -26,31 +26,19 @@ function get_go_pkg
     end
 end
 
-set vim_go \
-    github.com/klauspost/asmfmt/cmd/asmfmt@master \
-    github.com/go-delve/delve/cmd/dlv@master \
-    github.com/kisielk/errcheck@master \
+set gonvim \
     github.com/davidrjenni/reftools/cmd/fillstruct@master \
-    github.com/rogpeppe/godef@master \
     golang.org/x/tools/cmd/goimports@master \
-    golang.org/x/lint/golint@master \
-    honnef.co/go/tools/cmd/staticcheck@latest \
     github.com/fatih/gomodifytags@master \
-    golang.org/x/tools/cmd/gorename@master \
-    github.com/jstemmer/gotags@master \
-    golang.org/x/tools/cmd/guru@master \
-    github.com/josharian/impl@master \
-    honnef.co/go/tools/cmd/keyify@master \
-    github.com/fatih/motion@master \
-    github.com/koron/iferr@master
-
+    github.com/segmentio/golines \
+    mvdan.cc/gofumpt/gofumports \
+    mvdan.cc/gofumpt
 set gotests github.com/cweill/gotests/... 
 set wire github.com/google/wire/cmd/wire
 set grpc \
     google.golang.org/protobuf/cmd/protoc-gen-go \
     google.golang.org/grpc/cmd/protoc-gen-go-grpc \
     github.com/golang/protobuf/{proto,protoc-gen-go}
-
 set lint_lsp github.com/nametake/golangci-lint-langserver
 
-set go_pkgs $vim_go $gotests $wire $grpc $lint_lsp
+set go_pkgs $gonvim $gotests $wire $grpc $lint_lsp
