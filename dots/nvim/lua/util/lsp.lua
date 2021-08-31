b1425a54f3237516869ec486946c5ce2813cb78a
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-08-31 11:32:06 +0800

    [nvim] imporve lspconfig

--- dots/nvim/lua/util/lsp.lua
+++ dots/nvim/lua/util/lsp.lua
@@ -1,67 +1,48 @@
 local lspconfig = require("lspconfig")
 local augroup = require("util/shim").augroup
 local autocmd = require("util/shim").autocmd
-local set_highlight = require("util/shim").set_highlight
 local sign_define = require("util/shim").sign_define
 local plugin = require("util/plugin")
 
 plugin.use("neovim/nvim-lspconfig")
 plugin.use("ray-x/lsp_signature.nvim")
 
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr) -- function(client, bufnr)
 	local function buf_set_keymap(...)
 		vim.api.nvim_buf_set_keymap(bufnr, ...)
 	end
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
-
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
 	-- Mappings.
 	local opts = { noremap = true, silent = true }
-	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap("n", "[g", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-	buf_set_keymap("n", "]g", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
 	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 	buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
 	buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
 	buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
 	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 	buf_set_keymap("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+	buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
 	buf_set_keymap("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-
-	-- Set some keybinds conditional on server capabilities
-	if client.resolved_capabilities.document_formatting then
-		buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-	end
-	if client.resolved_capabilities.document_range_formatting then
-		buf_set_keymap("v", "<leader>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-	end
-
-	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
-		set_highlight("LspReferenceRead", { cterm = "bold", ctermbg = "red", guibg = "LightYellow" })
-		set_highlight("LspReferenceText", { cterm = "bold", ctermbg = "red", guibg = "LightYellow" })
-		set_highlight("LspReferenceWrite", { cterm = "bold", ctermbg = "red", guibg = "LightYellow" })
-		augroup("lsp_document_highlight", {
-			autocmd("CursorHold", "<buffer>", "lua vim.lsp.buf.document_highlight()"),
-			autocmd("CursorMoved", "<buffer>", "lua vim.lsp.buf.clear_references()"),
-			autocmd("CursorHold", "<buffer>", "lua vim.lsp.diagnostic.show_line_diagnostics()"),
-		})
-	end
+	buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
 	require("lsp_signature").on_attach()
-
 	augroup("lsp_format_on_save", {
-		autocmd("BufWritePre", "<buffer>", "lua vim.lsp.buf.formatting_sync(nil, 1000)"),
+		autocmd("BufWritePre", "<buffer>", "lua vim.lsp.buf.formatting_sync()"),
 	})
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = { "documentation", "detail", "additionalTextEdits" },
+}
 
 sign_define("LspDiagnosticsSignError", { text = "✗", texthl = "LspDiagnosticsError", linehl = "", numhl = "" })
 sign_define("LspDiagnosticsSignWarning", { text = "‼", texthl = "LspDiagnosticsWarning", linehl = "", numhl = "" })
@@ -75,6 +56,7 @@ local lsp = {}
 
 function lsp.set_config(name, config)
 	config["on_attach"] = on_attach
+	config["capabilities"] = capabilities
 	lspconfig[name].setup(config)
 end
 
