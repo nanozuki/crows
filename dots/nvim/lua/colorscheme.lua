9d4792180c9f68caaaccbde5a61c37400936088a
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-09-06 19:02:07 +0800

    [nvim] move some plugin to opt

--- dots/nvim/lua/colorscheme.lua
+++ dots/nvim/lua/colorscheme.lua
@@ -1,24 +1,35 @@
 vim.opt.termguicolors = true
-local function set_colorscheme(name, mode)
-	if mode == "dark" then
-		vim.opt.background = "dark"
-	else
-		vim.opt.background = "light"
-	end
 
-	if name == "gruvbox" then
+local colorschemes = {
+	gruvbox_light = function()
+		vim.cmd("packadd gruvbox-material")
+		vim.opt.background = "light"
 		vim.g["gruvbox_material_enable_italic"] = 1
 		vim.g["airline_theme"] = "gruvbox_material"
 		vim.cmd("colorscheme gruvbox-material")
-	elseif name == "nord" then
+	end,
+	gruvbox_dark = function()
+		vim.cmd("packadd gruvbox-material")
+		vim.opt.background = "dark"
+		vim.g["gruvbox_material_enable_italic"] = 1
+		vim.g["airline_theme"] = "gruvbox_material"
+		vim.cmd("colorscheme gruvbox-material")
+	end,
+	nord = function()
+		vim.cmd("packadd nord-vim")
 		vim.g["airline_theme"] = "nord"
 		vim.cmd("colorscheme nord")
-	elseif name == "edge" then
-		-- vim.g['edge_style'] = 'aura'
+	end,
+	edge_light = function()
+		vim.cmd("packadd edge")
 		vim.g["edge_enable_italic"] = 1
 		vim.g["airline_theme"] = "edge"
 		vim.cmd("colorscheme edge")
-	end
-	return
+	end,
+}
+
+local function set_colorscheme(name)
+	colorschemes[name]()
 end
-set_colorscheme("gruvbox", "light")
+
+set_colorscheme("gruvbox_light")
