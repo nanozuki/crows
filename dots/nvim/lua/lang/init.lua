4b9680890a24e3ad8bcf44da3f57263b72b58835
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-12-01 17:44:01 +0800

    [nvim] use format.nvim

--- dots/nvim/lua/lang/init.lua
+++ dots/nvim/lua/lang/init.lua
@@ -2,6 +2,29 @@ local feature = require('fur.feature')
 
 local lang = feature:new('lang')
 lang.source = 'lua/lang/init.lua'
+lang.plugins = {
+  {
+    'lukas-reineke/format.nvim',
+    config = function()
+      local eslint = { cmd = { 'npx eslint --fix' } }
+      require('format').setup({
+        ['*'] = { { cmd = { "sed -i 's/[ \t]*$//'" } } }, -- remove trailing whitespace
+        go = { { cmd = { 'goimports -w' } } },
+        lua = { { cmd = { 'stylua' } } },
+        typescript = { eslint },
+        javascript = { eslint },
+        typescriptreact = { eslint },
+        javascriptreact = { eslint },
+      })
+      local augroup = require('lib.util').augroup
+      local autocmd = require('lib.util').autocmd
+      augroup('format_on_save', {
+        autocmd('BufWritePost', '*', 'FormatWrite'),
+      })
+    end,
+  },
+}
+
 lang.children = {
   require('lang/lua'),
   require('lang/go'),
