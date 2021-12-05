276f3aeea36494375b66e7aca8a7461b24c99449
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-12-05 11:10:35 +0800

    [nvim] improve structure

--- dots/nvim/lua/features/ui/init.lua
+++
@@ -1,6 +0,0 @@
-local crows = require('crows')
-
-crows.execute('features/ui/colors.lua')
-crows.execute('features/ui/filetree.lua')
-crows.execute('features/ui/statusline.lua')
-crows.execute('features/ui/tabline.lua')
