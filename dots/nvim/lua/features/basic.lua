a79824930ffa14e922a087a4d15b28b1006af53a
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-12-06 15:04:32 +0800

    [nvim] misc fix

--- dots/nvim/lua/features/basic.lua
+++ dots/nvim/lua/features/basic.lua
@@ -1,8 +1,6 @@
 local crows = require('crows')
 local util = require('lib.util')
 
-local basic = {}
-
 vim.g.mapleader = ' '
 vim.opt.linebreak = true
 vim.opt.showbreak = '->'
@@ -12,12 +10,8 @@ vim.opt.relativenumber = true
 vim.opt.colorcolumn = '120'
 vim.opt.modelines = 1
 
-crows.map('Open terminal', 'n', '<leader>tt', ':terminal<CR>')
+crows.map('Open terminal', 'n', '<leader>tt', ':tabnew | terminal<CR>')
 crows.map('To normal mode in terminal', 't', '<Esc>', util.termcode([[<C-\><C-N>]]))
-
-function basic.renew_auto_session_cwd()
-  require('auto-session-library').conf.last_loaded_session = nil
-end
 
 crows.use_plugin({
   'rmagatti/auto-session',
@@ -29,9 +23,7 @@ crows.use_plugin({
     })
     local u = require('lib.util')
     u.augroup('renew_auto_session_cwd', {
-      u.autocmd('DirChanged', 'global', 'lua require("features.basic").renew_auto_session_cwd()'),
+      u.autocmd('DirChanged', 'global', 'lua require("auto-session-library").conf.last_loaded_session = nil'),
     })
   end,
 })
-
-return basic
