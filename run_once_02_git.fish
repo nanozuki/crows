f024b6c1f80cde80c6a3458e88d2cec42b7b69f3
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-12-08 13:50:13 +0800

    migrate install/update script

--- run_once_02_git.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-if not test -f $XDG_CONFIG_HOME/git/config_local
-    touch $XDG_CONFIG_HOME/git/config_local
-end
---
+++ utils.fish
@@ -0,0 +1,83 @@
+function setUx
+    set -Ux $argv
+    or return 0
+end
+
+function add_path
+    fish_add_path $argv
+    or return 0
+end
+
+function brew_install
+    brew install $argv
+    or return 0
+end
+
+function paru_install
+    paru -S --needed --noconfirm $argv
+end
+
+function find_git_repo_path
+    set repo (string trim -rc '.git' $argv[1]) 
+    set words (string match -r "(git@|https?://)(.*)/(.*)" $repo)
+    if test (count $words) -lt 4
+        echo "invalid repository path"
+        return 1
+    end
+    set ppath "$HOME/Projects/$words[3]"
+    if test $words[2] = "git@"
+        set ppath (string replace : / $ppath)
+    end
+    set path "$ppath/$words[4]"
+    echo $path
+end
+
+function git_latest
+    set repo_path (find_git_repo_path $argv[1])
+    if test -e $repo_path
+        set cur_dir (pwd)
+        cd $repo_path
+        set output (git pull); or cd $cur_dir && return 1
+        cd $cur_dir
+        if test (echo $output | grep 'Already up to date')
+            return 1
+        end
+    else
+        git clone $argv[1] $repo_path
+        or return 0
+    end
+    echo $repo_path
+end
+
+function macos_sync_lua_lsp
+    brew_install ninja
+    set repo https://github.com/sumneko/lua-language-server.git
+    set repo_path (git_latest $repo); or return 1
+    if test -z $repo_path
+        return 0
+    end
+
+    set cur_dir (pwd)
+    cd $repo_path
+    git submodule update --init --recursive
+    cd 3rd/luamake
+    compile/install.sh
+    cd ../..
+    ./3rd/luamake/luamake rebuild
+    cd $cur_dir
+end
+
+function macos_sync_zls
+    set repo https://github.com/zigtools/zls.git
+    set repo_path (git_latest $repo); or return 1
+    if test -z $repo_path
+        return 0
+    end
+
+    set cur_dir (pwd)
+    cd $repo_path
+    git submodule update --init --recursive
+    zig build -Drelease-safe
+    ln -sf (realpath ./zig-out/bin/zls) /usr/local/bin/zls
+    cd $cur_dir
+end
