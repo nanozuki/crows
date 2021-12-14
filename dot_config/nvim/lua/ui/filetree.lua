a5fba6099593ee06b5e2451b0aa6d3e0621a3bfc
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-12-14 16:36:25 +0800

    [nvim] improve nvim-tree's setup

--- dot_config/nvim/lua/ui/filetree.lua
+++ dot_config/nvim/lua/ui/filetree.lua
@@ -5,13 +5,11 @@ crows.use_plugin({
   'kyazdani42/nvim-tree.lua',
   requires = 'kyazdani42/nvim-web-devicons',
   config = function()
-    require('nvim-tree.view').View.winopts.signcolumn = 'auto'
     require('nvim-tree').setup({
       disable_netrw = false,
       update_cwd = true,
-      diagnostics = {
-        enable = true,
-      },
+      diagnostics = { enable = true },
+      view = { signcolumn = 'auto' },
     })
     require('crows').map('Toggle filetree', 'n', '<Leader>fl', ':NvimTreeToggle<CR>')
   end,
