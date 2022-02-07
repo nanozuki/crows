7262524d6ca1d3b0a990176f3aa31bae6f8e7e07
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-02-07 16:46:38 +0800

    [nvim] only load plugin files to lua lsp in config dir

--- dot_config/nvim/lua/lang/lua.lua
+++ dot_config/nvim/lua/lang/lua.lua
@@ -2,6 +2,15 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
+local function workspace_files()
+  local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+  if cwd == '~/.config/nvim' then
+    -- Make the server aware of Neovim runtime files, only in config cwd
+    return vim.api.nvim_get_runtime_file('', true)
+  end
+  return nil
+end
+
 local sumneko_lua_settings = {
   cmd = { 'lua-language-server' },
   settings = {
@@ -17,8 +26,7 @@ local sumneko_lua_settings = {
         globals = { 'vim' },
       },
       workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
+        library = workspace_files(),
         maxPreload = 5000,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
