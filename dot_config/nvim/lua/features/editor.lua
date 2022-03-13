9a433189a6c507b5256b7f2821c740afdd4adfb7
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-03-13 16:09:46 +0800

    [nvim] fix auto pair

--- dot_config/nvim/lua/features/editor.lua
+++ dot_config/nvim/lua/features/editor.lua
@@ -9,16 +9,10 @@ crows.use_plugin('kshenoy/vim-signature')
 crows.use_plugin('mg979/vim-visual-multi')
 
 -- autopairs
-crows.use_plugin({
-  'Raimondi/delimitMate',
-  config = function()
-    vim.cmd([[
-      let delimitMate_expand_cr = 1
-      let delimitMate_expand_space = 1
-      let delimitMate_jump_expansion = 1
-    ]])
-  end,
-})
+crows.use_plugin('Raimondi/delimitMate')
+vim.g.delimitMate_expand_cr = 1
+vim.g.delimitMate_expand_space = 1
+vim.g.delimitMate_jump_expansion = 1
 
 -- surround edit
 crows.use_plugin('machakann/vim-sandwich')
