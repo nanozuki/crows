0df0ce07110ff225fb6cbe8f52c2e2897dc5d756
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-03-31 17:57:30 +0800

    make bootstarp usable

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,16 +1,6 @@
 -- Nanozuki Vim Config
 
 local crows = require('crows')
-crows.plugin.bootstrap()
-
-require('features')
-require('ui')
-require('lang')
-
 crows.setup({
-  reload_modules = {
-    'features',
-    'lang',
-    'ui',
-  },
+  modules = { 'features', 'lang', 'ui' },
 })
