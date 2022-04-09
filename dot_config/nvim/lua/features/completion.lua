4e0251c28bf93ceeaab077b35c5b965f90d7aeb8
crows <pt.wenhan@gmail.com>
2022-04-09 22:33:12 +0800

    [nvim] improve wiler.nvim, use autopairs.nvim

--- dot_config/nvim/lua/features/completion.lua
+++ dot_config/nvim/lua/features/completion.lua
@@ -4,25 +4,29 @@ local completion = { plugins = {} }
 completion.plugins[1] = {
   'gelguy/wilder.nvim',
   config = function()
-    vim.cmd([[
-      call wilder#setup({'modes': [':', '/', '?']})
-      call wilder#set_option('pipeline', [
-        \   wilder#branch(
-        \     wilder#cmdline_pipeline({
-        \       'language': 'python',
-        \       'fuzzy': 1,
-        \     }),
-        \     wilder#python_search_pipeline({
-        \       'pattern': wilder#python_fuzzy_pattern(),
-        \       'sorter': wilder#python_difflib_sorter(),
-        \       'engine': 're',
-        \     }),
-        \   ),
-        \ ])
-      call wilder#set_option('renderer', wilder#popupmenu_renderer({
-        \ 'highlighter': wilder#basic_highlighter(),
-        \ }))
-    ]])
+    local wilder = require('wilder')
+    wilder.setup({
+      modes = { ':', '/', '?' },
+    })
+    wilder.set_option('pipeline', {
+      wilder.branch(
+        wilder.cmdline_pipeline({
+          language = 'python',
+          fuzzy = 1,
+        }),
+        wilder.python_search_pipeline({
+          pattern = wilder.python_fuzzy_pattern(),
+          sorter = wilder.python_difflib_sorter(),
+          engine = 're',
+        })
+      ),
+    })
+    wilder.set_option(
+      'renderer',
+      wilder.popupmenu_renderer({
+        highlighter = wilder.basic_highlighter(),
+      })
+    )
   end,
 }
 
