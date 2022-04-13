2686e52f459ea019c08cfa1c47dec24c34b9494f
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-04-13 15:34:07 +0800

    [nvim] improve keymapping for terminal

--- dot_config/nvim/lua/features/basic.lua
+++ dot_config/nvim/lua/features/basic.lua
@@ -40,8 +40,8 @@ basic.plugins = {
 }
 
 basic.post = function()
-  crows.key.map('Open terminal in new tab', 'n', '<leader>tt', ':tabnew | terminal<CR>')
-  crows.key.map('To normal mode in terminal', 't', '<Esc>', util.termcode([[<C-\><C-N>]]))
+  crows.key.map('Open terminal in new tab', 'n', '<leader>tt', ':terminal<CR>')
+  crows.key.map('To normal mode in terminal', 't', '<C-K>', util.termcode([[<C-\><C-N>]]))
 end
 
 return basic
