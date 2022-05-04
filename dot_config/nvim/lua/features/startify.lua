b5e037e3002f818720e163067816ead79e69bb85
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-05-04 19:29:56 +0800

    [nvim] improve startify

--- dot_config/nvim/lua/features/startify.lua
+++ dot_config/nvim/lua/features/startify.lua
@@ -91,7 +91,7 @@ section.footer = {
 
 startify.config = {
   layout = {
-    { type = 'padding', val = 16 },
+    { type = 'padding', val = 4 },
     section.header,
     { type = 'padding', val = 4 },
     section.buttons,
