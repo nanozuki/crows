5d97f120ca27a79431741ee8a8cd15692ad282fc
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-08-17 14:11:56 +0800

    [nvim] use tabby v2

--- dot_config/nvim/lua/features/tabline.lua
+++ dot_config/nvim/lua/features/tabline.lua
@@ -1,67 +1,45 @@
 local tabby_config = function()
   local palette = require('features.theme').palette
-  local filename = require('tabby.module.filename')
-  local tabname = function(tabid)
-    local number = vim.api.nvim_tabpage_get_number(tabid)
-    local name = require('tabby.tab').get_name(tabid)
-    return string.format('%d: %s', number, name)
-  end
-  local cwd = function()
-    return ' ' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t') .. ' '
-  end
-  local line = {
-    hl = { fg = palette.fg, bg = palette.bg },
-    layout = 'active_wins_at_tail',
-    head = {
-      { cwd, hl = { fg = palette.bg, bg = palette.accent, style = 'italic' } },
-      { '', hl = { fg = palette.accent, bg = palette.bg } },
-    },
-    active_tab = {
-      label = function(tabid)
-        return {
-          '  ' .. tabname(tabid) .. ' ',
-          hl = { fg = palette.bg, bg = palette.accent_sec, style = 'bold' },
-        }
-      end,
-      left_sep = { '', hl = { fg = palette.accent_sec, bg = palette.bg } },
-      right_sep = { '', hl = { fg = palette.accent_sec, bg = palette.bg } },
-    },
-    inactive_tab = {
-      label = function(tabid)
-        return {
-          '  ' .. tabname(tabid) .. ' ',
-          hl = { fg = palette.fg, bg = palette.bg_sec, style = 'bold' },
-        }
-      end,
-      left_sep = { '', hl = { fg = palette.bg_sec, bg = palette.bg } },
-      right_sep = { '', hl = { fg = palette.bg_sec, bg = palette.bg } },
-    },
-    top_win = {
-      label = function(winid)
-        return {
-          '  ' .. filename.unique(winid) .. ' ',
-          hl = { fg = palette.fg, bg = palette.bg_sec },
-        }
-      end,
-      left_sep = { '', hl = { fg = palette.bg_sec, bg = palette.bg } },
-      right_sep = { '', hl = { fg = palette.bg_sec, bg = palette.bg } },
-    },
-    win = {
-      label = function(winid)
-        return {
-          '  ' .. filename.unique(winid) .. ' ',
-          hl = { fg = palette.fg, bg = palette.bg_sec },
-        }
-      end,
-      left_sep = { '', hl = { fg = palette.bg_sec, bg = palette.bg } },
-      right_sep = { '', hl = { fg = palette.bg_sec, bg = palette.bg } },
-    },
-    tail = {
-      { '', hl = { fg = palette.accent_sec, bg = palette.bg } },
-      { '  ', hl = { fg = palette.bg, bg = palette.accent_sec } },
-    },
+  local theme = {
+    line = { fg = palette.fg, bg = palette.bg },
+    head = { fg = palette.bg, bg = palette.accent, style = 'italic' },
+    current_tab = { fg = palette.bg, bg = palette.accent_sec, style = 'bold' },
+    tab = { fg = palette.fg, bg = palette.bg_sec, style = 'bold' },
+    win = { fg = palette.fg, bg = palette.bg_sec },
+    tail = { fg = palette.bg, bg = palette.accent_sec, style = 'bold' },
   }
-  require('tabby').setup({ tabline = line })
+  local cwd = ' ' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t') .. ' '
+  vim.opt.showtabline = 2
+  require('tabby.tabline').set(function(line)
+    return {
+      { { cwd, hl = theme.head }, line.sep('', theme.head, theme.line) },
+      line.tabs().foreach(function(tab)
+        local hl = tab.is_current() and theme.current_tab or theme.tab
+        return {
+          line.sep('', hl, theme.line),
+          tab.is_current() and '' or '',
+          string.format('%s:', tab.number()),
+          tab.name(),
+          line.sep('', hl, theme.line),
+          margin = ' ',
+          hl = hl,
+        }
+      end),
+      line.spacer(),
+      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+        return {
+          line.sep('', theme.win, theme.line),
+          win.is_current() and '' or '',
+          win.buf_name(),
+          line.sep('', theme.win, theme.line),
+          margin = ' ',
+          hl = theme.win,
+        }
+      end),
+      { line.sep('', theme.tail, theme.line), { '  ', hl = theme.tail } },
+      hl = theme.line,
+    }
+  end, {})
 end
 
 ---@type Feature
@@ -71,6 +49,7 @@ tabline.plugins = {
   {
     'nanozuki/tabby.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
+    branch = 'v2-preview',
     config = tabby_config,
   },
 }
