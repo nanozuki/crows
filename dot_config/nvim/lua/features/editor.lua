482a71e8509e5d341d429251f808221d89b46b9f
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-09-11 22:51:38 +0800

    [nvim] fix treesitter

--- dot_config/nvim/lua/features/editor.lua
+++ dot_config/nvim/lua/features/editor.lua
@@ -85,6 +85,7 @@ editor.plugins[#editor.plugins + 1] = {
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = 'all',
+      ignore_install = { 'phpdoc' },
       highlight = {
         enable = true,
         disable = function(lang, bufnr)
