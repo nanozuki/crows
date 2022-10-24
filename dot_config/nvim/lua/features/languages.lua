1ff0b550035628c190c8015155d42649d3c15a9b
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-10-24 15:34:04 +0800

    feat(nvim): clearify config of lua-language-server

--- dot_config/nvim/lua/features/languages.lua
+++ dot_config/nvim/lua/features/languages.lua
@@ -44,45 +44,7 @@ local lua = {
     fmt.by_formatter.lua = { fmt.formatters.stylua }
   end,
   post = function()
-    local runtime_path = vim.split(package.path, ';')
-    table.insert(runtime_path, 'lua/?.lua')
-    table.insert(runtime_path, 'lua/?/init.lua')
-
-    local function workspace_files()
-      local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
-      if cwd == '~/.config/nvim' then
-        -- Make the server aware of Neovim runtime files, only in config cwd
-        return vim.api.nvim_get_runtime_file('', true)
-      end
-      return nil
-    end
-
-    local sumneko_lua_settings = {
-      cmd = { 'lua-language-server' },
-      settings = {
-        Lua = {
-          runtime = {
-            version = 'LuaJIT',
-            path = runtime_path,
-          },
-          completion = {
-            autoRequire = false,
-          },
-          diagnostics = {
-            globals = { 'vim' },
-          },
-          workspace = {
-            library = workspace_files(),
-            maxPreload = 5000,
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    }
-    lsp.set_config('sumneko_lua', sumneko_lua_settings)
+    lsp.set_config('sumneko_lua', { Lua = { telemetry = { enable = false }, runtime = { version = 'LuaJIT' } } })
   end,
 }
 
