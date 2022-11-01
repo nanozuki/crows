bcfd387d41230bca458845b44dd8d501c5ad266e
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-11-01 17:29:05 +0800

    feat(neovim): disable tresitter when open big file

--- dot_config/nvim/lua/features/editor.lua
+++ dot_config/nvim/lua/features/editor.lua
@@ -91,6 +91,20 @@ editor.plugins[#editor.plugins + 1] = {
       ignore_install = { 'phpdoc' },
       highlight = {
         enable = true,
+        highlight = {
+          enable = true,
+          disable = function(lang, bufnr)
+            if lang == 'html' and vim.api.nvim_buf_line_count(bufnr) > 500 then
+              return true
+            end
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, 3, false)) do
+              if #line > 500 then
+                return true
+              end
+            end
+            return false
+          end,
+        },
       },
     })
   end,
