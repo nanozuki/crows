8bf0ca02cbc66342a1c3fabc4242b4270a340e9a
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-02-06 14:54:02 +0800

    feat(script): add fish entries for deno scripts

---
+++ dot_config/fish/functions/init-env.fish
@@ -0,0 +1,3 @@
+function init-env
+    deno run --allow-run --allow-net --allow-env ~/.config/fish/deno/init_env.ts gpg rime go rust ocaml
+end
---
+++ dot_config/fish/functions/update-env.fish
@@ -0,0 +1,3 @@
+function update-env
+    deno run --allow-run --allow-net --allow-env ~/.config/fish/deno/update_env.ts gpg rime go rust ocaml
+end
