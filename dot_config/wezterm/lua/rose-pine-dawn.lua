883023395bd1a4fe7ec6e47f06d659336c9b8424
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-02-08 16:49:23 +0800

    feat(wezterm): improving colors

--- dot_config/wezterm/lua/rose-pine-dawn.lua
+++ dot_config/wezterm/lua/rose-pine-dawn.lua
@@ -1,35 +1,44 @@
 -- rose-pine-dawn
-
 local M = {}
 
 local palette = {
   base = '#faf4ed',
+  surface = '#fffaf3',
   overlay = '#f2e9e1',
   muted = '#9893a5',
+  subtle = '#797593',
   text = '#575279',
+  love = '#b4637a',
+  gold = '#ea9d34',
+  rose = '#d7827e',
+  pine = '#286983',
+  foam = '#56949f',
   iris = '#907aa9',
+  highlight_low = '#f4ede8',
+  highlight_med = '#dfdad9',
+  highlight_high = '#cecacd',
 }
 
 local active_tab = {
-  bg_color = palette.text,
-  fg_color = palette.base,
+  bg_color = palette.overlay,
+  fg_color = palette.text,
 }
 
 local inactive_tab = {
-  bg_color = palette.base,
+  bg_color = palette.highlight_med,
   fg_color = palette.text,
 }
 
 function M.colors()
   return {
     tab_bar = {
-      background = palette.base,
+      background = palette.highlight_med,
       active_tab = active_tab,
       inactive_tab = inactive_tab,
       inactive_tab_hover = active_tab,
       new_tab = inactive_tab,
       new_tab_hover = active_tab,
-      inactive_tab_edge = palette.muted, -- (Fancy tab bar only)
+      inactive_tab_edge = palette.highlight_high, -- (Fancy tab bar only)
     },
   }
 end
