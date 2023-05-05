c280890d12f7de9068042b010f8c1f2738514e81
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-05-05 16:32:03 +0800

    fix(neovim): user command for filetype go

--- dot_config/nvim/ftplugin/go.lua
+++ dot_config/nvim/ftplugin/go.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_buf_create_user_command(0, 'GoModTidy', function(_)
-  vim.execute([[!go mod tidy]], true)
+  vim.cmd([[!go mod tidy]])
   vim.cmd([[LspRestart]])
 end, {})
 vim.api.nvim_buf_create_user_command(0, 'GoBuild', '!go build -o /dev/null', {})
--- dot_config/nvim/ftplugin/gomod.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_buf_create_user_command(0, 'GoModTidy', function(_)
-  vim.cmd([[:!go mod tidy]])
-  vim.cmd([[LspRestart]])
-end, {})
