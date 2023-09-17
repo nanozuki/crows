14a1f3e6ddb650d79abafcd2cff6691273295b55
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-09-17 12:24:08 +0800

    refact(fish): clearify the config

--- dot_config/fish/functions/gitget.fish
+++ dot_config/fish/functions/gitget.fish
@@ -1,3 +1,19 @@
+function find_git_repo_path
+    set trim_dot_git (string match -r '^(.*?)(\.git)?$' $argv[1])
+    set words (string match -r "(git@|https?://)(.*)/(.*)" $trim_dot_git[2])
+    if test (count $words) -lt 4
+        echo "invalid repository path"
+        return 1
+    end
+    set dirpath (string replace -ar '~' '_' $words[3])
+    set ppath "$HOME/Projects/$dirpath"
+    if test $words[2] = "git@"
+        set ppath (string replace : / $ppath)
+    end
+    set path "$ppath/$words[4]"
+    echo $path
+end
+
 function gitget
     if test (count $argv) -lt 1
         echo "[usage: gitget <repository>]"
--- dot_config/fish/functions/init-env.fish
+++
@@ -1,3 +0,0 @@
-function init-env
-    deno run --allow-run --allow-net --allow-env ~/.config/fish/deno/init_env.ts $argv
-end
