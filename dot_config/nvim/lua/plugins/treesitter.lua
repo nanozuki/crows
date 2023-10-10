357a4145248828d68f0b166ba7e098c5f18de680
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-10-10 18:34:07 +0800

    feat(neovim): use noice, notify, incline. remove treesitter-context, barbecue

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -79,12 +79,4 @@ return {
       })
     end,
   },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    event = { 'BufReadPost', 'BufNewFile' },
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-      require('treesitter-context').setup({})
-    end,
-  },
 }
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,5 @@
+local values = require('config.values')
+
 return {
   -- improve vim select/input UI
   {
@@ -36,5 +38,50 @@ return {
     opts = {},
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    enabled = values.use_noice,
+    opts = {
+      views = {
+        cmdline_popup = {
+          position = { row = 0, col = -2 },
+          relative = 'cursor',
+        },
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nuircarriga/nvim-notify',
+    },
+  },
+  {
+    'nuircarriga/nvim-notify',
+    lazy = true,
+    config = function()
+      local notify = require('notify')
+      vim.notify = notify
+      ---@diagnostic disable
+      notify.setup({
+        render = 'wrapped-compact',
+        stages = 'fade',
+      })
+      ---@diagnostic enable
+    end,
   },
 }
