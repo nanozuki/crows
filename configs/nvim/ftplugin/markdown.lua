f71dbff2a11157f8972375f11770e7c9193a3e12
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-11-08 11:34:43 +0800

    fix(neovim): opt in ftplugin/*.lua

--- configs/nvim/ftplugin/markdown.lua
+++ configs/nvim/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
-vim.opt.colorcolumn = '80'
-vim.opt.textwidth = 80
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt_local.colorcolumn = '80'
+vim.opt_local.textwidth = 80
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
