1dcfa79f770f2eed435369c19cac35eac5fa4094
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-11-30 16:16:46 +0800

    feat(home-manager): NixOS 23.11 Released

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of crows";
 
   inputs = {
-    stable-nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    stable-nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
