fa4d4ca8fa46bdff6df6ce21e27872e59818209f
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-12-24 22:20:33 +0800

    fix(home-manager): daily update

--- clips/common.nix
+++ clips/common.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, sspkgs, ... }:
 {
   apps.bat.enable = true;
   apps.git.enable = true;
@@ -72,14 +72,8 @@
         src = pkgs.fishPlugins.fzf-fish.src;
       }
       {
-        # TODO: move to git.sr.ht/~fubuki/stratosphere
         name = "nix-fish";
-        src = pkgs.fetchFromGitHub {
-          owner = "kidonng";
-          repo = "nix.fish";
-          rev = "ad57d970841ae4a24521b5b1a68121cf385ba71e";
-          sha256 = "GMV0GyORJ8Tt2S9wTCo2lkkLtetYv0rc19aA5KJbo48=";
-        };
+        src = sspkgs.fishplugins-nix-fish;
       }
     ];
   };
