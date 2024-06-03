d9f3fc0a809c10127ccfdd0a76b16edcc5de42ce
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-06-03 14:48:26 +0800

    feat(nvim): update config of 'lazydev.nvim' and 'tabby.nvim'

--- configs/nvim/lua/plugins/tabline.lua
+++ configs/nvim/lua/plugins/tabline.lua
@@ -12,37 +12,39 @@ end
 local function config()
   local theme = make_theme()
   vim.opt.showtabline = 2
-  require('tabby.tabline').set(function(line)
-    local cwd = ' ' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t') .. ' '
-    return {
-      { { cwd, hl = theme.head }, line.sep('', theme.head, theme.line) },
-      line.tabs().foreach(function(tab)
-        local hl = tab.is_current() and theme.current_tab or theme.tab
-        return {
-          line.sep('', hl, theme.line),
-          tab.is_current() and '󰆤' or '󰆣',
-          string.format('%s:', tab.number()),
-          tab.name(),
-          line.sep('', hl, theme.line),
-          margin = ' ',
-          hl = hl,
-        }
-      end),
-      line.spacer(),
-      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-        return {
-          line.sep('', theme.win, theme.line),
-          win.is_current() and '' or '',
-          win.buf_name(),
-          line.sep('', theme.win, theme.line),
-          margin = ' ',
-          hl = theme.win,
-        }
-      end),
-      { line.sep('', theme.tail, theme.line), { '  ', hl = theme.tail } },
-      hl = theme.line,
-    }
-  end, {})
+  require('tabby').setup({
+    line = function(line)
+      local cwd = ' ' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t') .. ' '
+      return {
+        { { cwd, hl = theme.head }, line.sep('', theme.head, theme.line) },
+        line.tabs().foreach(function(tab)
+          local hl = tab.is_current() and theme.current_tab or theme.tab
+          return {
+            line.sep('', hl, theme.line),
+            tab.is_current() and '󰆤' or '󰆣',
+            string.format('%s:', tab.number()),
+            tab.name(),
+            line.sep('', hl, theme.line),
+            margin = ' ',
+            hl = hl,
+          }
+        end),
+        line.spacer(),
+        line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+          return {
+            line.sep('', theme.win, theme.line),
+            win.is_current() and '' or '',
+            win.buf_name(),
+            line.sep('', theme.win, theme.line),
+            margin = ' ',
+            hl = theme.win,
+          }
+        end),
+        { line.sep('', theme.tail, theme.line), { '  ', hl = theme.tail } },
+        hl = theme.line,
+      }
+    end,
+  })
   vim.keymap.set('n', '<leader>wp', ':Tabby pick_window<CR>', { desc = 'Select a window' })
   vim.keymap.set('n', '<leader>tr', ':Tabby rename_tab ', { desc = 'Rename current tab' })
 end
