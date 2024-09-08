4c6f9ae748a65824a0b6ec334052137ca54d4efe
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-09-08 16:44:49 +0800

    fix(nest): input method setting

--- machines/nest/configuration.nix
+++ machines/nest/configuration.nix
@@ -89,7 +89,8 @@
     LC_TIME = "en_US.UTF-8";
   };
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5 = {
       addons = [ pkgs.fcitx5-rime ];
       waylandFrontend = true;
