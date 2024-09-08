82158f7332931fbf92269a779b0b7853aecb2016
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-09-08 16:24:12 +0800

    feat(nest): user DHCP

--- machines/nest/configuration.nix
+++ machines/nest/configuration.nix
@@ -55,14 +55,7 @@
   # Enable networking
   networking = {
     hostName = "homeport";
-    interfaces = {
-      enp10s0.ipv4.addresses = [{
-        address = "192.168.1.21";
-        prefixLength = 24;
-      }];
-    };
-    defaultGateway = "192.168.1.2";
-    nameservers = [ "192.168.1.2" ];
+    useDHCP = true;
   };
 
   # Set your time zone.
