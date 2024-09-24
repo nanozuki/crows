bd91d1d8bccb6a410dd086bf27a6d788061a7354
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-09-24 09:28:52 +0800

    feat(pica): add a gcp component

--- homes/pica.nix
+++ homes/pica.nix
@@ -13,7 +13,9 @@
 
     home.packages = with pkgs; [
       awscli2
-      google-cloud-sdk
+      (google-cloud-sdk.withExtraComponents [
+        google-cloud-sdk.components.gke-gcloud-auth-plugin
+      ])
       zstd
     ];
 
