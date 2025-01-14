fdf5cf3ae329f4302070af1ff94515940519590b
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-01-14 13:51:29 +0800

    feat(nest): rename to services.pulseaudio

--- machines/nest/configuration.nix
+++ machines/nest/configuration.nix
@@ -110,7 +110,7 @@
   services.printing.enable = true;
 
   # Enable pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
