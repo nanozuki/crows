d43a4e85bbbb0c4e1589bb9acd8b95b639de0d4f
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-01-23 11:02:21 +0800

    feat(neovim): learn some config from kickstart.nvim

--- configs/nvim/lua/plugins/search.lua
+++ configs/nvim/lua/plugins/search.lua
@@ -17,24 +17,15 @@ return {
           file_ignore_patterns = { '.*%.pb.*%.go', '.*api.yaml', 'swagger.yaml' },
         },
       })
-      vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Find files' })
-      vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Grep in files' })
-      vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'Find buffer' })
-      vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Find help' })
-      vim.keymap.set('n', '<leader>fm', require('telescope.builtin').marks, { desc = 'Find mark' })
-      vim.keymap.set(
-        'n',
-        '<leader>fc',
-        require('telescope.builtin').command_history,
-        { desc = 'Find command in history' }
-      )
-      vim.keymap.set(
-        'n',
-        '<leader>fy',
-        require('telescope.builtin').lsp_workspace_symbols,
-        { desc = 'Find lsp symbol' }
-      )
-      vim.keymap.set('n', '<leader>ft', require('telescope.builtin').treesitter, { desc = 'List item by treesitter' })
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Grep in files' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffer' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
+      vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = 'Find mark' })
+      vim.keymap.set('n', '<leader>fc', builtin.command_history, { desc = 'Find command in history' })
+      vim.keymap.set('n', '<leader>fy', builtin.lsp_workspace_symbols, { desc = 'Find lsp symbol' })
+      vim.keymap.set('n', '<leader>ft', builtin.treesitter, { desc = 'List item by treesitter' })
       vim.keymap.set('n', '<leader>z', require('telescope').extensions.z.list, { desc = 'Find path by z' })
     end,
   },
