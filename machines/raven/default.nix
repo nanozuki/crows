c20e9c32f585c3f8eedaf4839a833ede2ebe39ec
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-05-24 09:51:33 +0200

    feat: upgrade to 25.05!

--- machines/raven/default.nix
+++ machines/raven/default.nix
@@ -81,8 +81,8 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 6;
-
-  system.activationScripts.postUserActivation.text = ''
+  system.primaryUser = "crows";
+  system.activationScripts.reload-settings.text = ''
     # activateSettings -u will reload the settings from the database and apply them to the current session,
     # so we do not need to logout and login again to make the changes take effect.
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
