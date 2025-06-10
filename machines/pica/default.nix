2f18dd9d35e5b8bd90445a335358c8a26a043c2a
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-06-10 16:44:16 +0200

    feat: update config for nix-dawrin

--- machines/pica/default.nix
+++ machines/pica/default.nix
@@ -51,11 +51,15 @@
   # nixpkgs.config.allowUnfree = true;
 
   # Necessary for using flakes on this ystem.
-  nix.settings.experimental-features = "nix-command flakes";
-  nix.gc = {
-    automatic = true;
-    interval = { Weekday = 0; Hour = 10; Minute = 0; };
-    options = "--delete-older-than 30d";
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+    };
+    gc = {
+      automatic = true;
+      interval = { Weekday = 0; Hour = 10; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
   };
 
   # Create /etc/zshrc that loads the nix-darwin environment.
