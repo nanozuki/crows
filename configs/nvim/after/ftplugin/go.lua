029004a5b586bd36832d6846a1fc7eb456d13a07
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-11-03 15:07:42 +0100

    feat(neovim): add keymap to organize imports

--- configs/nvim/after/ftplugin/go.lua
+++
@@ -1,8 +0,0 @@
-local bufnr = vim.api.nvim_get_current_buf()
-
-vim.keymap.set('n', '<leader>oi', function()
-  vim.lsp.buf.code_action({
-    context = { diagnostics = {}, only = { 'source.organizeImports' } },
-    apply = true,
-  })
-end, { desc = 'Organize imports', buffer = bufnr })
