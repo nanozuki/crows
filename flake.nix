bba9dd1eb76510465c4c1f16f4b746b353ac245e
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-11-25 22:04:23 +0100

    feat: add machine config of treepie

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "Home Manager configuration of crows";
+  description = "Nix configurations of crows";
 
   inputs = {
-    stable-nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    stable-nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -76,6 +76,13 @@
               clips = import ./clips nixpkgs.legacyPackages."aarch64-darwin" "aarch64-darwin";
             };
           }
+        ];
+        specialArgs = { inherit self inputs; };
+      };
+      nixosConfigurations.treepie = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./overlays.nix
+          ./machines/treepie/configuration.nix
         ];
         specialArgs = { inherit self inputs; };
       };
